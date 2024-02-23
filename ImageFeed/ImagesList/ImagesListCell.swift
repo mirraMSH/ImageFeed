28996fb7ba1ad9ec88994f91be39c9a4670548b2
Maria <manya0705@yandex.ru>
2024-02-23 21:02:44 +0300

    экран загрузки + скролл лента

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Мария Шагина on 23.02.2024.
+//
+
+import Foundation
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Мария Шагина on 14.01.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
