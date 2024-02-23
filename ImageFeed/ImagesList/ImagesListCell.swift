7665f652a0e1092dd5b754c6b68e6ca194cdc1eb
Maria <manya0705@yandex.ru>
2024-02-23 21:30:19 +0300

     экран загрузки и скрол лента

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,4 +5,14 @@
 //  Created by Мария Шагина on 23.02.2024.
 //
 
-import Foundation
+import UIKit
+
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
+}
