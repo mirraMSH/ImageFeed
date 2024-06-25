7af1f7a03142b99a2adc920eb35958d0d15a5603
Maria <manya0705@yandex.ru>
2024-06-25 19:49:32 +0300

    sprint 13. webview test commit

--- ImageFeedTests/WebViewViewControllerSpy.swift
+++ ImageFeedTests/WebViewViewControllerSpy.swift
@@ -5,4 +5,23 @@
 //  Created by Мария Шагина on 25.06.2024.
 //
 
+import ImageFeed
 import Foundation
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+
+    var loadRequestCalled: Bool = false
+
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+
+    }
+}
